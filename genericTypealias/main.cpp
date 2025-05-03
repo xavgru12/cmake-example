2bf78f9fb9da1bd3d206924786093ad140fb76f0
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-05-03 16:00:54 +0200

    first way to reverse a vector

--- genericTypealias/main.cpp
+++ genericTypealias/main.cpp
@@ -9,9 +9,25 @@ int main() {
     std::cout << "object1 depth: " << object1.getDepth() << std::endl;
     std::cout << "object2 depth: " << object2.getDepth() << std::endl;
 
-    std::vector<GenericTypeParamType> types;
-    types.push_back(object1);
-    types.push_back(object2);
+    std::vector<GenericTypeParamType*> types;
+    types.push_back(&object1);
+    types.push_back(&object2);
+
+    std::vector<GenericTypeParamType*> mutableNominalParams(
+    types.begin(), types.end());
+
+    unsigned maxDepth = 1;
+
+
+    for (auto parameter = mutableNominalParams.rbegin();
+         parameter != mutableNominalParams.rend(); ) {
+      if ((*parameter)->getDepth() == maxDepth) {
+        auto it_to_erase = std::next(parameter).base(); // base() gives forward iterator
+        parameter = std::reverse_iterator(mutableNominalParams.erase(it_to_erase));
+      } else {
+        ++parameter;
+      }
+    }
 
     return 0;
 }
