dcbf4e54225e1b23c2ced8847bb5aaad7ac48680
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-05-03 14:51:41 +0200

    rename

--- genericTypealias/main.cpp
+++ genericTypealias/main.cpp
@@ -1,11 +1,11 @@
 #include "GenericTypeParamType.h"
 
 int main() {
-    GenericTypeParamType param1(0);
-    GenericTypeParamType param2(1);
+    GenericTypeParamType object1(0);
+    GenericTypeParamType object2(1);
 
-    std::cout << "param1 depth: " << param1.getDepth() << std::endl;
-    std::cout << "param2 depth: " << param2.getDepth() << std::endl;
+    std::cout << "object1 depth: " << object1.getDepth() << std::endl;
+    std::cout << "object2 depth: " << object2.getDepth() << std::endl;
 
     return 0;
 }
